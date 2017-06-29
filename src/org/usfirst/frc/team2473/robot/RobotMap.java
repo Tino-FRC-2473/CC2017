1ad691d88b71b3bb2cef934d850de0c2a00257a2
ksank <ksank@DESKTOP-8FOQU90.fuhsd.org>
2017-06-28 20:43:57 -0700

    Basic Robot Java Project Template w/ DriveTrain added.

---
+++ src/org/usfirst/frc/team2473/robot/RobotMap.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc.team2473.robot;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+	public static final int FRONT_RIGHT = 0;
+	public static final int FRONT_LEFT = 0;
+	public static final int BACK_RIGHT = 0;
+	public static final int BACK_LEFT = 0;
+	public static final int GYRO = 0;
+	public static final int SERVO = 0;
+}
