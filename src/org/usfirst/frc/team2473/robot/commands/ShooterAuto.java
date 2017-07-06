b9f5c7517e515e4ce0724bcffd61ba3464a98a5b
aashrav shetty <aashrav.shetty01@gmail.com>
2017-07-05 20:15:08 -0700

    shooter auto added

--- src/org/usfirst/frc/team2473/robot/commands/ShooterAuto.java
+++ src/org/usfirst/frc/team2473/robot/commands/ShooterAuto.java
@@ -7,30 +7,32 @@ import edu.wpi.first.wpilibj.command.Command;
 /**
  *
  */
-public class ClimberRun extends Command {
+public class ShooterAuto extends Command {
+	
 
-    public ClimberRun() {
+    public ShooterAuto() {
         // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    	requires(Robot.climber);
+        requires(Robot.shooter);
+    	
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	Robot.shooter.initDefaultCommand();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
     protected void end() {
-    	
     	
     }
 
