b650aec2a2a5047f6f29a2c9bb5a802bd8341ca5
Julia <crystallineblade@gmail.com>
2017-07-08 19:00:27 +0000

    post day test - serial stuffs

--- scan_test.py
+++ scan_test.py
@@ -3,26 +3,30 @@ import itertools
 import time
 from threading import Thread
 
-angle = []
-distance = []
+def loopGetData(sweep):
+        print("\nBEGIN INFINITE LOOP")
+        while(True):
+                time.sleep(8)
+                getData(itertools.islice(sweep.get_scans(), 3))
+                
 
 def getData(i):
-        for scan in i:
-                print("scan")
-                s = scan[0]
-                for dataSample in s:
-                        angle.append(dataSample[0]/1000.0)
-                        distance.append(dataSample[1])
-                        print(str(dataSample[0]/1000.0) + " " + str(dataSample[1]))
-                break
-        time.sleep(2)
+        with open("scans.csv", "a") as f:
+                print(".")
+                f.write("NEW GETDATA")
+                for scan in i:
+                        f.write("\nscan" + "\n")
+                        s = scan[0]
+                        for dataSample in s:
+                                f.write(str(dataSample[0]/1000.0) + " " + str(dataSample[1]) + "\n")
 
 
 with Sweep('/dev/ttyUSB0') as sweep:
         sweep.set_motor_speed(1)
         sweep.set_sample_rate(2000)
+        print("lidar connection being established")
         sweep.start_scanning()
-        t = Thread(target=getData(itertools.islice(sweep.get_scans(),3)))
-        t.start()
+        print("connection established")
+        Thread(target=loopGetData(sweep)).start()
 
         
---
+++ serial_test.py
@@ -0,0 +1,29 @@
+import itertools
+import time
+import serial
+
+def loopGetData():
+        print("\nBEGIN INFINITE LOOP")
+        with serial.Serial('/dev/ttyUSB0', timeout=10) as ser:
+                print("after with")
+                ser.write(str(ord("D")) + str(ord("S")))
+                print(ser.read(10))
+                
+loopGetData()
+
+
+def getData(i):
+        with open("scans.csv", "a") as f:
+                print(".")
+                f.write("NEW GETDATA")
+                for scan in i:
+                        f.write("\nscan" + "\n")
+                        s = scan[0]
+                        for dataSample in s:
+                                f.write(str(dataSample[0]/1000.0) + " " + str(dataSample[1]) + "\n")
+
+'''
+sweep.set_motor_speed(1)
+sweep.set_sample_rate(2000)
+sweep.start_scanning()
+'''
