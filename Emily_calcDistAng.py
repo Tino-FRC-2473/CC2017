1d65b8d689878002857e660c3fb38d2d5bfb5978
Work <work@MacBook-Pro-2.local>
2017-07-12 20:57:21 -0700

    Did useless things to determine overlapping rectangles, added comments

--- Emily_calcDistAng.py
+++ Emily_calcDistAng.py
@@ -23,7 +23,7 @@ pinDistToCenter = 0;
 
 # angle;
 
-def calcDist(length):
+def calcDist(length): #the length of the rectangle
     #the distance and size is inversely proportional
     #dist * rectSize = constant
     if(length != 0):
@@ -43,12 +43,73 @@ def calcPinDist():
     #SOMEHOW GET PINX and PINY
     return math.fabs(pinX - SCREEN_WIDTH / 2);
 
+#only need top left and bottom right!!
 def pinPosition(x1, y1, x2, y2, x3, y3, x4, y4):
     x = (x1 + x2 + x3 + x4) / 4.0;
     y = (y1 + y2 + y3 + y4) / 4.0;
     return (int(x), int(y))
 
 
+"""returns true if the rectangle overlaps given the x and y coordinates
+of the top left and bottom right corners"""
+"""def overlaps(x1, y1, x2, y2, x3, y3, x4, y4):
+    #starting on rectangle 1 and going left to right, top to bottom
+    #then rectangle 2
+    #then 0 case
+
+    #first rectangle
+    #check x1, y1
+    if((x3 < x1 and x1 < x4) #x1 is between x3 and x4
+     and (y3 < y1 and y1 < y4)): #y1 is between y3 and y4
+    	return True
+    #check x2, y1
+    elif((x3 < x2 and x2 < x4) #x2 is between x3 and
+     and (y3 < y1 and y1 < y4)): #y1 is between y3 and y4
+    	return True
+    #check x1, y2
+    elif((x3 < x1 and x1 < x4) #x1 is between x3 and x4
+	 and (y3 < y2 and y2 < y4)): #y2 is between y3 and y4
+    	return True
+    #check x2, y2
+    elif((x3 < x2 and x2 < x4) #x2 is between x3 and x4
+     and (y3 < y2 and y2 < y4)): #y1 is between y3 and y4
+    	return True
+
+    #second rectangle
+	#check x3, y3
+    elif((x1 < x3 and x3 < x2) #x3 is between x1 and x2
+     and (y1 < y3 and y3 < y2)): #y3 is between y1 and y2
+    	return True
+	#check x4, y3
+    elif((x1 < x4 and x4 < x2) #x4 is between x1 and x2
+     and (y1 < y3 and y3 < y2)): #y3 is between y1 and y2
+    	return True
+	#check x3, y4
+    elif((x1 < x3 and x3 < x2) #x3 is between x1 and x2
+     and (y1 < y4 and y4 < y2)): #y4 is between y1 and y2
+    	return True
+    #check x4, y4
+    elif((x1 < x4 and x4 < x2) #x4 is between x1 and x2
+     and (y1 < y4 and y4 < y2)): #y4 is between y1 and y2
+    	return True
+
+    #the zero cases
+    #first rect crosses vertically
+    elif((x3 < x1 and x1 < x4) #x1 is between x3 and x4
+     and (x3 < x2 and x2 < x4) #x2 is between x3 and x4
+     and (y1 < y3 and y3 < y2) #y3 is between y1 and y2
+     and (y1 < y4 and y4 < y2)): #y4 is between y1 and y2
+    	return True
+    #first rect crosses horizontally
+    elif((x1 < x3 and x3 < x2) #x3 is between x1 and x2
+     and (x1 < x4 and x4 < x2) #x4 is between x1 and x2
+     and (y3 < y1 and y1 < y4)) #y1 is between y3 and y4
+     and (y3 < y2 and y2 < y4)): #y2 is between y3 and y4
+		return True
+
+	#if alll the above fails
+	return False
+"""
 camera = cv2.VideoCapture(0)
 _, frame = camera.read()
 SCREEN_HEIGHT, SCREEN_WIDTH = frame.shape[:2]
@@ -59,16 +120,19 @@ while True:
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    zero_red = np.array([0, 100, 100]);
-    low_red = np.array([10, 255, 255]);
-    high_red = np.array([160, 100, 100]);
-    pi_red = np.array([180, 255, 255]);
-
+    # zero_red = np.array([0, 100, 100]);
+    # low_red = np.array([10, 255, 255]);
+    # high_red = np.array([160, 100, 100]);
+    # pi_red = np.array([180, 255, 255]);
     #make mask
-    maskLow = cv2.inRange(hsv, zero_red, low_red)
-    maskHigh = cv2.inRange(hsv, high_red, pi_red)
-    mask = maskLow + maskHigh
+    # maskLow = cv2.inRange(hsv, zero_red, low_red)
+    # maskHigh = cv2.inRange(hsv, high_red, pi_red)
+    # mask = maskLow + maskHigh
 
+    low_white = np.array([0,0,100]);
+    high_white = np.array([360, 10, 255]);
+    mask = cv2.inRange(hsv, low_white, high_white);
+    
     # #green thresholds
     # low_green = np.array([40, 100, 100])
     # high_green = np.array([80, 255, 255])
@@ -104,6 +168,7 @@ while True:
             my = y
             mw = w
             mh = h
+
 
 
     #draw rectangles on two biggest green part found
