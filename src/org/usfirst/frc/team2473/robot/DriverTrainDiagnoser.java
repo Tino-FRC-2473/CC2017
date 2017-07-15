ab12c94f9669f287db772572913f8b2ab16a8b32
Neel Sankaran <Neel Sankaran>
2017-07-15 14:42:53 -0700

    Finished drivetrain simul test

--- src/org/usfirst/frc/team2473/robot/DriverTrainDiagnoser.java
+++ src/org/usfirst/frc/team2473/robot/DriverTrainDiagnoser.java
@@ -18,6 +18,14 @@ public class DriverTrainDiagnoser extends Diagnoser {
 	private String keyflp;
 	private String keyblp;
 	private String keybrp;
+	private String keyfrs;
+	private String keyfls;
+	private String keybls;
+	private String keybrs;
+	private String keyfrc;
+	private String keyflc;
+	private String keyblc;
+	private String keybrc;
 	
 	private Joystick stick;
 	
@@ -27,26 +35,48 @@ public class DriverTrainDiagnoser extends Diagnoser {
 	private double rpmfl = 0.0;
 	private double rpmbr = 0.0;
 	private double rpmbl = 0.0;
+	double torquefr = 0.0;
+	double torquefl = 0.0;
+	double torquebr = 0.0;
+	double torquebl = 0.0;
 	
 	private double encoders = 1600;
 	
-	private double maxtorque = 0.7;
+	private final double MAX_TORQUE = 0.03;
+	private final double MAX_CURRENT = 10.0;
 	
-	public DriverTrainDiagnoser(CANTalon fr, CANTalon fl, CANTalon bl, CANTalon br, String keyfrp, 
-								String keyflp, String keybrp, String keyblp, String keyfre, 
-								String keyfle, String keybre, String keyble, Joystick stick){
+	private double SpeedMultiplier = 1.0;
+	
+	public DriverTrainDiagnoser(CANTalon fr, CANTalon fl, CANTalon bl, CANTalon br, Joystick stick, 
+								String keyfrp, String keyflp, String keybrp, String keyblp, 
+								String keyfre, String keyfle, String keybre, String keyble, 
+								String keyfrs, String keyfls, String keybrs, String keybls, 
+								String keyfrc, String keyflc, String keybrc, String keyblc){
 		this.fr = fr;
 		this.fl = fl;
 		this.bl = bl;
 		this.br = br;
+		//encoder values
 		this.keyfre = keyfre;
 		this.keyfle = keyfle;
 		this.keybre = keybre;
 		this.keyble = keyble;
+		//power values
 		this.keyfrp = keyfrp;
 		this.keyflp = keyflp;
 		this.keybrp = keybrp;
 		this.keyblp = keyblp;
+		//speed values(CANTalon.getSpeed())
+		this.keyfrp = keyfrs;
+		this.keyflp = keyfls;
+		this.keybrp = keybrs;
+		this.keyblp = keybls;
+		//input current values
+		this.keyfrp = keyfrc;
+		this.keyflp = keyflc;
+		this.keybrp = keybrc;
+		this.keyblp = keyblc;
+		//the joystick
 		this.stick = stick;
 	}
 
@@ -115,23 +145,38 @@ public class DriverTrainDiagnoser extends Diagnoser {
 	public void RunSimultaneousTest() {
 		// TODO Auto-generated method stub
 		//current power speed
-		double torquefr;
-		double torquefl;
-		double torquebr;
-		double torquebl;
+		double pastrpmfr;
+		double pastrpmfl;
+		double pastrpmbr;
+		double pastrpmbl;
+		double currentfr = DataBase.getDeviceValue(keyfrc);
+		double currentfl = DataBase.getDeviceValue(keyflc);
+		double currentbr = DataBase.getDeviceValue(keybrc);
+		double currentbl = DataBase.getDeviceValue(keyblc);
+		
 		if(DiagnosticThread.getTime()%1000 == 0){
-			double pastrpmfr = rpmfr;
-			double pastrpmfl = rpmfl;
-			double pastrpmbr = rpmbr;
-			double pastrpmbl = rpmbl;
-			rpmfr = ((fr.getSpeed()/100)/this.EcnoderTicksPerRotation)*(2*Math.PI);
-			rpmfl = ((fl.getSpeed()/100)/this.EcnoderTicksPerRotation)*(2*Math.PI);
-			rpmbr = ((br.getSpeed()/100)/this.EcnoderTicksPerRotation)*(2*Math.PI);
-			rpmbl = ((bl.getSpeed()/100)/this.EcnoderTicksPerRotation)*(2*Math.PI);
+			pastrpmfr = rpmfr;
+			pastrpmfl = rpmfl;
+			pastrpmbr = rpmbr;
+			pastrpmbl = rpmbl;
+			rpmfr = ((DataBase.getDeviceValue(keyfrs)/100)/this.EcnoderTicksPerRotation)*(2*Math.PI);
+			rpmfl = ((DataBase.getDeviceValue(keyfls)/100)/this.EcnoderTicksPerRotation)*(2*Math.PI);
+			rpmbr = ((DataBase.getDeviceValue(keybrs)/100)/this.EcnoderTicksPerRotation)*(2*Math.PI);
+			rpmbl = ((DataBase.getDeviceValue(keybls)/100)/this.EcnoderTicksPerRotation)*(2*Math.PI);
 			torquefr = (rpmfr - pastrpmfr);
 			torquefl = (rpmfl - pastrpmfl);
 			torquebr = (rpmbr - pastrpmbr);
 			torquebl = (rpmbl - pastrpmbl);
+		}
+		
+		if(torquefr >= this.MAX_TORQUE || torquefl >= this.MAX_TORQUE || 
+		   torquebr >= this.MAX_TORQUE || torquebl >= this.MAX_TORQUE ||
+		   currentfr >= this.MAX_CURRENT || currentfl >= this.MAX_CURRENT ||
+		   currentbr >= this.MAX_CURRENT || currentbl >= this.MAX_CURRENT){
+			System.out.println("FrontRightMotor: high torque");
+			this.SpeedMultiplier -= 0.1;
+		}else{
+			this.SpeedMultiplier = 1.0;
 		}
 	}
 	
--- src/org/usfirst/frc/team2473/robot/ServoDiagnoser.java
+++ src/org/usfirst/frc/team2473/robot/ServoDiagnoser.java
@@ -5,10 +5,12 @@ import edu.wpi.first.wpilibj.Servo;
 public class ServoDiagnoser extends Diagnoser{
 	Servo servo;
 	String key;
+	Double range;
 	
-	public ServoDiagnoser(Servo servo,String key){
+	public ServoDiagnoser(Servo servo,String key, Double range){
 		this.servo = servo;
 		this.key = key;
+		this.range = range;
 		Diagnostics.addToQue(this);
 	}
 
