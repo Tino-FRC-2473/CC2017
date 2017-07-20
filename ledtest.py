a7c2dadc8727a2f6238da8dee912b0c56f56fcf6
Julia Shuieh <Julia Shuieh>
2017-07-19 19:10:26 -0700

    added LED test

---
+++ ledtest.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+
+red = 21
+white = 20
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(red,GPIO.OUT)
+GPIO.setup(white,GPIO.OUT)
+
+GPIO.output(red,GPIO.LOW)
+GPIO.output(white,GPIO.LOW)
+
+try: 
+	while 1:
+		GPIO.output(red,GPIO.HIGH)
+		GPIO.output(white,GPIO.LOW)
+		time.sleep(0.5)
+		GPIO.output(red,GPIO.LOW)
+		GPIO.output(white,GPIO.HIGH)
+		time.sleep(0.5)
+except KeyboardInterrupt:
+	GPIO.cleanup()
