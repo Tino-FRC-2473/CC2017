d2724c4a14cedebd5a6db3c31e4ea3796a127b05
mtamer492 <milesat60@gmail.com>
2017-07-22 14:09:53 -0700

    changed code to support new architecture

--- src/org/usfirst/frc/team2473/robot/Robot.java
+++ src/org/usfirst/frc/team2473/robot/Robot.java
@@ -1,13 +1,21 @@
 package org.usfirst.frc.team2473.robot;
 
-import edu.wpi.first.wpilibj.IterativeRobot;import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import org.usfirst.frc.team2473.framework.components.Devices;
+import org.usfirst.frc.team2473.framework.components.Trackers;
+import org.usfirst.frc.team2473.framework.readers.ControlsReader;
+import org.usfirst.frc.team2473.framework.readers.DeviceReader;
+import org.usfirst.frc.team2473.framework.trackers.JoystickTracker;
+import org.usfirst.frc.team2473.framework.trackers.JoystickTracker.Type;
 import org.usfirst.frc.team2473.robot.commands.JoystickControl;
 import org.usfirst.frc.team2473.robot.subsystems.DriveTrain;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -18,26 +26,25 @@ import org.usfirst.frc.team2473.robot.subsystems.DriveTrain;
  */
 public class Robot extends IterativeRobot {
 
+	DeviceReader reader;
+	Timer robotControlLoop;
+	private boolean timerRunning = true;
 	public static OI oi;
 	public static DriveTrain driveTrain;
 	public static JoystickControl joycom;
 	Command autonomousCommand;
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
+
 	@Override
 	public void robotInit() {
+		addDevices();
+		addTrackers();
+		reader = new DeviceReader();
+		reader.start();
+		
 		driveTrain = new DriveTrain();
 		joycom = new JoystickControl();
-		oi = new OI();
 	}
 
-	/**
-	 * This function is called once each time the robot enters Disabled mode.
-	 * You can use it to reset any subsystem information you want to clear when
-	 * the robot is disabled.
-	 */
 	@Override
 	public void disabledInit() {
 
@@ -48,24 +55,11 @@ public class Robot extends IterativeRobot {
 		Scheduler.getInstance().run();
 	}
 
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString code to get the auto name from the text box below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional commands to the
-	 * chooser code above (like the commented example) or additional comparisons
-	 * to the switch structure below with additional strings & commands.
-	 */
 	@Override
 	public void autonomousInit() {
+		timerRunning = true;
 	}
 
-	/**
-	 * This function is called periodically during autonomous
-	 */
 	@Override
 	public void autonomousPeriodic() {
 		Scheduler.getInstance().run();
@@ -73,28 +67,38 @@ public class Robot extends IterativeRobot {
 
 	@Override
 	public void teleopInit() {
-		// This makes sure that the autonomous stops running when
-		// teleop starts running. If you want the autonomous to
-		// continue until interrupted by another command, remove
-		// this line or comment it out.
+		timerRunning = false;
 		joycom.start();
 		if (autonomousCommand != null)
 			autonomousCommand.cancel();
 	}
 
-	/**
-	 * This function is called periodically during operator control
-	 */
 	@Override
 	public void teleopPeriodic() {
-		Scheduler.getInstance().run();
+		if(!timerRunning){
+			robotControlLoop.scheduleAtFixedRate(new TimerTask(){
+				@Override
+				public void run(){
+					Scheduler.getInstance().run();
+				}
+			}, 0, 20);
+			timerRunning=true;
+		}
+		ControlsReader.getInstance().updateAll();
 	}
 
-	/**
-	 * This function is called periodically during test mode
-	 */
 	@Override
 	public void testPeriodic() {
 		LiveWindow.run();
+	}
+	public void addDevices(){
+		Devices.getInstance().addTalon(RobotMap.FRONT_LEFT);
+		Devices.getInstance().addTalon(RobotMap.FRONT_RIGHT);
+		Devices.getInstance().addTalon(RobotMap.BACK_LEFT);
+		Devices.getInstance().addTalon(RobotMap.BACK_RIGHT);
+	}
+	public void addTrackers(){
+		Trackers.getInstance().addTracker(new JoystickTracker(ControlsMap.STEERING_WHEEL_X,ControlsMap.STEERING_WHEEL,Type.X));
+		Trackers.getInstance().addTracker(new JoystickTracker(ControlsMap.THROTTLE_Y,ControlsMap.THROTTLE,Type.Y));
 	}
 }
