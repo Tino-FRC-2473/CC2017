f933c915082d6942d7f7d21f1cd4102ff0e44b00
mtamer492 <milesat60@gmail.com>
2017-08-01 15:31:15 -0700

    added setRightPow() and setLeftPow()

--- src/org/usfirst/frc/team2473/robot/commands/DriveStraight.java
+++ src/org/usfirst/frc/team2473/robot/commands/DriveStraight.java
@@ -3,8 +3,10 @@ package org.usfirst.frc.team2473.robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
 
 import org.usfirst.frc.team2473.framework.Database;
+import org.usfirst.frc.team2473.framework.components.Devices;
 import org.usfirst.frc.team2473.robot.ControlsMap;
 import org.usfirst.frc.team2473.robot.Robot;
+import org.usfirst.frc.team2473.robot.RobotMap;
 
 public class DriveStraight extends Command {
 
@@ -24,7 +26,7 @@ public class DriveStraight extends Command {
 			System.out.println(Robot.piDriveTrain.getGyro().getYaw());
 		}
 		else {
-			Robot.driveTrain.driveArcade(squareWithSign(Database.getInstance().getNumeric(ControlsMap.THROTTLE_Z)),Database.getInstance().getNumeric(ControlsMap.STEERING_WHEEL_X));
+			Robot.piDriveTrain.drive(squareWithSign(Database.getInstance().getNumeric(ControlsMap.THROTTLE_Z)),Database.getInstance().getNumeric(ControlsMap.STEERING_WHEEL_X));
 			System.out.println(Database.getInstance().getNums());
 		}
 	}
@@ -40,6 +42,19 @@ public class DriveStraight extends Command {
 			return 0;
 		}
     }
+	
+	private void setRightPow(double pow){
+		
+		Devices.getInstance().getTalon(RobotMap.FRONT_RIGHT).set(pow);
+		Devices.getInstance().getTalon(RobotMap.BACK_RIGHT).set(pow);
+
+	}
+	private void setLeftPow(double pow){
+		
+		Devices.getInstance().getTalon(RobotMap.FRONT_LEFT).set(pow);
+		Devices.getInstance().getTalon(RobotMap.BACK_LEFT).set(pow);
+
+	}
 
 	@Override
 	protected boolean isFinished() {
