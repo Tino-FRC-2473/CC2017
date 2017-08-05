d21631b91d711ad9169afc7ed8e2de5ceeb27648
unknown <ksankaran2107@gmail.com>
2017-08-05 01:00:51 -0700

    Quick fixes after Thursday's tests.

--- src/org/usfirst/frc/team2473/robot/commands/DriveStraight.java
+++ src/org/usfirst/frc/team2473/robot/commands/DriveStraight.java
@@ -12,7 +12,10 @@ import org.usfirst.frc.team2473.robot.RobotMap;
 public class DriveStraight extends Command {
 
 	private double maxPow = 0.8;
-	private double deadzone = 0.04;
+	private double wheelDeadzone = 0.04;
+	private double throttleDeadzone = 0.05;
+	//WARNING!!!!!!!!!!!!!!!!!!
+	//Cannot run code without initializing maxWheelX value correctly
 	private double maxWheelX = 0;
 	
 	public DriveStraight() {
@@ -26,30 +29,68 @@ public class DriveStraight extends Command {
 
 	@Override
 	protected void execute() {
-//		if (Math.abs(Database.getInstance().getNumeric(ControlsMap.THROTTLE_Z)) <= 0.04) {
-//			Robot.piDriveTrain.drive(Database.getInstance().getNumeric(ControlsMap.THROTTLE_Z),
-//					Robot.piDriveTrain.getAngleRate());
-//			System.out.println(Robot.piDriveTrain.getGyro().getYaw());
 		if(Math.abs(Database.getInstance().getNumeric(ControlsMap.THROTTLE_Z)) > 0.04){
-			turn(squareRtWithSign(Database.getInstance().getNumeric(ControlsMap.THROTTLE_Z)), Database.getInstance().getNumeric(ControlsMap.STEERING_WHEEL_X));
+			turn(squareWithSign(Database.getInstance().getNumeric(ControlsMap.THROTTLE_Z)), Database.getInstance().getNumeric(ControlsMap.STEERING_WHEEL_X));
 		}
 		else {
-			Robot.piDriveTrain.drive(squareRtWithSign(Database.getInstance().getNumeric(ControlsMap.THROTTLE_Z)),
+			Robot.piDriveTrain.drive(squareWithSign(Database.getInstance().getNumeric(ControlsMap.THROTTLE_Z)),
 					Database.getInstance().getNumeric(ControlsMap.STEERING_WHEEL_X));
 			System.out.println(Database.getInstance().getNums());
 		}
 	}
 
-	private double squareRtWithSign(double d) {
+	private double squareWithSign(double d) {
 		if (d >= 0.04) {
-			return -Math.sqrt(d);
+			System.out.println(-d*d);
+			return -d*d;
 		} else if (d <= -0.04) {
-			return Math.sqrt(d);
+			System.out.println(d*d);
+			return d*d;
 		} else {
 			return 0;
 		}
 	}
 
+	private void turn(double pow, double turn) {
+		if(Math.abs(pow) < maxPow)
+		{
+			pow = maxPow * Math.signum(pow);
+		}
+		// if right
+		if (turn > wheelDeadzone) {
+			//if throttle forward
+			if (pow > throttleDeadzone) {
+				setLeftPow(0.4 / (maxWheelX - wheelDeadzone) * (turn - wheelDeadzone) + 0.5);
+				setRightPow(-0.4 / (maxWheelX - wheelDeadzone) * (turn - wheelDeadzone) + 0.5);
+			} else if (pow < -throttleDeadzone) {
+				setLeftPow(-(0.4 / (maxWheelX - wheelDeadzone) * (turn - wheelDeadzone) + 0.5));
+				setRightPow(-(-0.4 / (maxWheelX - wheelDeadzone) * (turn - wheelDeadzone) + 0.5));
+			} else {
+//				setLeftPow(pow);
+//				setRightPow(-pow);
+				System.out.println(Database.getInstance().getNumeric(ControlsMap.STEERING_WHEEL_X));
+			}
+		}
+		// if left
+		else if (turn < -wheelDeadzone) {
+			if (pow > throttleDeadzone) {
+				setLeftPow(0.4 / (maxWheelX - wheelDeadzone) * (turn + wheelDeadzone) + 0.5);
+				setRightPow(-0.4 / (maxWheelX - wheelDeadzone) * (turn + wheelDeadzone) + 0.5);
+			} else if (pow < -throttleDeadzone) {
+				setLeftPow(-(0.4 / (maxWheelX - wheelDeadzone) * (turn + wheelDeadzone) + 0.5));
+				setRightPow(-(-0.4 / (maxWheelX - wheelDeadzone) * (turn + wheelDeadzone) + 0.5));
+			} else {
+//				setLeftPow(-pow);
+//				setRightPow(pow);
+				System.out.println((double)Database.getInstance().getNumeric(ControlsMap.STEERING_WHEEL_X));
+			}
+		}
+		else {
+			setLeftPow(pow);
+			setRightPow(pow);
+		}
+	}
+	
 	private void setRightPow(double pow) {
 		if (pow > maxPow)
 			pow = maxPow;
@@ -68,47 +109,6 @@ public class DriveStraight extends Command {
 		Devices.getInstance().getTalon(RobotMap.FRONT_LEFT).set(pow);
 		Devices.getInstance().getTalon(RobotMap.BACK_LEFT).set(pow);
 
-	}
-
-	private void turn(double pow, double turn) {
-		if (pow > maxPow)
-			pow = maxPow;
-		if (pow < -maxPow)
-			pow = -maxPow;
-		// right turn
-		if (turn > deadzone) {
-			if (pow > deadzone) {
-				setLeftPow(0.4 / (maxWheelX - deadzone) * (turn - deadzone) + 0.5);
-				setRightPow(-0.4 / (maxWheelX - deadzone) * (turn - deadzone) + 0.5);
-				// setLeftPow(0.9*pow);
-				// setRightPow(0.1*pow);
-			} else if (pow < -deadzone) {
-				setLeftPow(-(0.4 / (maxWheelX - deadzone) * (turn - deadzone) + 0.5));
-				setRightPow(-(-0.4 / (maxWheelX - deadzone) * (turn - deadzone) + 0.5));
-				// setLeftPow(0.1*pow);
-				// setRightPow(0.9*pow);
-			} else {
-				setLeftPow(pow);
-				setRightPow(-pow);
-			}
-		}
-		// left turn
-		else if (turn < -deadzone) {
-			if (pow > deadzone) {
-				setLeftPow(0.4 / (maxWheelX - deadzone) * (turn + deadzone) + 0.5);
-				setRightPow(-0.4 / (maxWheelX - deadzone) * (turn + deadzone) + 0.5);
-				// setLeftPow(0.1*pow);
-				// setRightPow(0.9*pow);
-			} else if (pow < -deadzone) {
-				setLeftPow(-(0.4 / (maxWheelX - deadzone) * (turn + deadzone) + 0.5));
-				setRightPow(-(-0.4 / (maxWheelX - deadzone) * (turn + deadzone) + 0.5));
-				// setLeftPow(0.9*pow);
-				// setRightPow(0.1*pow);
-			} else {
-				setLeftPow(-turn);
-				setRightPow(turn);
-			}
-		}
 	}
 
 	@Override
